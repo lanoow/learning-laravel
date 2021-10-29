fab576a1b43da4f30f683145b9663c2d0bd36275
Lanoow <joshsenderson159@gmail.com>
2021-10-30 00:21:31 +0300

    Day 1

---
+++ README.md
@@ -0,0 +1,20 @@
+# Learning Laravel
+## Preparation
+1. Choose an IDE to code in, I personally like **Visual Studio Code**
+2. Configure it to your preference
+3. If you're using **VS Code**, here are my **TOP** extensions for **Laravel**:
+	* [Laravel Extension Pack](https://marketplace.visualstudio.com/items?itemName=onecentlin.laravel-extension-pack) -> Provides very useful extensions for **Laravel**
+	
+## Installation
+I personally chose to install **Laravel** with **Composer** because I think it's the best choice for me
+
+	$ composer create-project laravel/laravel example-app
+	$ cd example-app
+	$ php artisan serve
+---
+But I also installed and the **Laravel Installer** just in case I need it
+
+	$ composer global require laravel/installer
+	$ laravel new example-app
+	$ cd example-app
+	$ php artisan serve	
---
+++ learning-laravel/resources/views/post.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=7">
+
+    <title>Post | Learning Laravel</title>
+
+    <link rel="stylesheet" href="https://cdn.blazingdev.xyz/tailwind.min.css">
+    <link rel="stylesheet" href="https://cdn.blazingdev.xyz/fontawesome/css/all.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+Antique:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+
+    <style>
+        .font { font-family: "Zen Kaku Gothic Antique", Arial, Helvetica, sans-serif !important; }
+    </style>
+</head>
+<body class="font overflow-x-hidden">
+    <div class="container mx-auto flex flex-col my-24">
+        <img class="md:rounded-xl w-full h-96 mx-auto object-cover" src="https://picsum.photos/1920/1080" alt="Blog Image">
+        <div class="flex flex-col my-8 mx-8 md:mx-0 space-y-4">
+            <span class="text-6xl text-gray-900 font-black">Blog Title</span>
+            <span class="text-2xl text-gray-700">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, sapiente laborum qui sint mollitia laboriosam cupiditate neque eum! Fuga quaerat perferendis architecto veritatis suscipit obcaecati, rerum libero dicta inventore ipsum.</span>
+        </div>
+
+        <div>
+            <a href="/" class="py-3 px-4 mx-8 md:mx-0 rounded-xl bg-gray-200 hover:bg-red-600 text-xl font-bold text-black hover:text-white transition duration-200 transform hover:scale-105"><i class="fas fa-home"></i> Go Home</a>
+        </div>
+    </div>
+</body>
+</html>
